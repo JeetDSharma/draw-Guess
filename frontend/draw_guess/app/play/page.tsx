05bbf01fbdfc5b2d6a67f018da8d9d5bb4be5d0d
JeetDSharma <jeetsharma2112@gmail.com>
2026-03-08 18:58:38 -0400

    layout changes

--- frontend/draw_guess/app/play/page.tsx
+++ frontend/draw_guess/app/play/page.tsx
@@ -1,9 +1,11 @@
 import CanvasComponent from "../../components/canvas/canvasComponent";
+import ChatboxComponent from "../../components/chatbox/chatboxComponent";
 
 const page = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-full gap-4">
       <CanvasComponent />
+      <ChatboxComponent />
     </div>
   );
 };
